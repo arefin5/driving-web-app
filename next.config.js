0ccdd5cce144b183451a88d3f16f2f69b9a4d2fa
arefin5 <71636237+arefin5@users.noreply.github.com>
2024-04-01 11:44:19 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
-const path = require('path');
-
 const nextConfig = {
   images: {
-    domains: ["localhost","www.driveshikhun.com", "driveshikhun.com"],
+    domains: ["localhost", "www.driveshikhun.com", "driveshikhun.com"],
   },
   env: {
     baseUrl: "https://backend.driveshikhun.com/api/v1",
   },
-  webpack: (config, { dev, isServer }) => {
-    // Enable persistent caching for webpack 5 in production
-    if (!dev && !isServer) {
-      config.cache = {
-        type: 'filesystem',
-        cacheDirectory: path.resolve(__dirname, '.webpack-cache'), // make it absolute
-      };
-    }
+  webpack: (config, { isServer }) => {
+    if (!isServer) config.resolve.fallback.fs = false;
     return config;
   },
 };
